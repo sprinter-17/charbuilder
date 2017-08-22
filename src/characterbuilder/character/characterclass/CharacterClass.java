5d535d582a7c1bb08d6175351ba911e77aaaed72
simon <simon@10.0.0.149>
2017-08-22 19:41:01 +1000

    Moved barbarian to delegate

--- src/characterbuilder/character/characterclass/CharacterClass.java
+++ src/characterbuilder/character/characterclass/CharacterClass.java
@@ -7,12 +7,11 @@ import characterbuilder.character.ability.Proficiency;
 import static characterbuilder.character.ability.Proficiency.*;
 import characterbuilder.character.ability.Skill;
 import static characterbuilder.character.ability.Skill.*;
-import characterbuilder.character.attribute.AbilityScore;
 import characterbuilder.character.attribute.Attribute;
 import characterbuilder.character.attribute.AttributeType;
 import static characterbuilder.character.attribute.AttributeType.*;
 import characterbuilder.character.attribute.IntAttribute;
-import characterbuilder.character.characterclass.barbarian.PrimalPath;
+import characterbuilder.character.characterclass.barbarian.Barbarian;
 import characterbuilder.character.characterclass.bard.BardicCollege;
 import characterbuilder.character.characterclass.cleric.DivineDomain;
 import characterbuilder.character.characterclass.druid.DruidCircle;
@@ -62,37 +61,7 @@ import java.util.stream.Stream;
 import org.w3c.dom.Node;
 
 public enum CharacterClass implements Attribute {
-    BARBARIAN(12, PRIMAL_PATH, STRENGTH, CONSTITUTION,
-        Arrays.asList(STRENGTH, CONSTITUTION), (cls, gen) -> {
-        gen.level(1).addAttributes(Proficiency.LIGHT_ARMOUR, Proficiency.MEDIUM_ARMOUR,
-            Proficiency.SHIELD, ALL_WEAPONS);
-        gen.level(1).addChoice(2, new AttributeChoice("Skill",
-            ANIMAL_HANDLING, ATHLETICS, INTIMIDATION, NATURE, PERCEPTION, SURVIVAL));
-        gen.level(1).addChoice(new EquipmentChoice("Primary Weapon")
-            .with(GREATEAXE).with(EquipmentCategory.MARTIAL_MELEE));
-        gen.level(1).addChoice(new EquipmentChoice("Secondary Weapon")
-            .with(new EquipmentSet(HANDAXE, 2))
-            .with(EquipmentCategory.SIMPLE_MELEE)
-            .with(EquipmentCategory.SIMPLE_RANGED));
-        gen.level(1).addEquipment(EXPLORER_PACK);
-        gen.level(1).addEquipment(JAVELIN, 4);
-        gen.level(1).addAttributes(RAGE, UNARMORED_DEFENCE_BARBARIAN);
-        gen.level(2).addAttributes(RECKLESS_ATTACK, DANGER_SENSE);
-        gen.level(3).addChoice(new AttributeChoice("Primal Path", PrimalPath.values()));
-        gen.cond(levels(4, 8, 12, 16, 19)).addChoice(2, new AbilityScoreOrFeatIncrease());
-        gen.level(5).addAttributes(EXTRA_ATTACK, FAST_MOVEMENT);
-        gen.level(7).addAttributes(FERAL_INSTINCTS);
-        gen.level(9).addAttributes(BRUTAL_CRITICAL);
-        gen.level(11).addAttributes(RELENTLESS_RAGE);
-        gen.level(15).addAttributes(PERSISTENT_RAGE);
-        gen.level(18).addAttributes(INDOMITABLE_MIGHT);
-        gen.level(20).addAction("Increase Str. and Con.", ch -> {
-            ch.getAttribute(STRENGTH, AbilityScore.class).setMax(24);
-            ch.getAttribute(STRENGTH, AbilityScore.class).addValue(4);
-            ch.getAttribute(CONSTITUTION, AbilityScore.class).setMax(24);
-            ch.getAttribute(CONSTITUTION, AbilityScore.class).addValue(4);
-        });
-    }),
+    BARBARIAN(new Barbarian()),
     BARD(8, BARDIC_COLLEGE, DEXTERITY, CHARISMA,
         Arrays.asList(CHARISMA, DEXTERITY), (cls, gen) -> {
         gen.level(1).addSpellCasting("Bard", CHARISMA, cls, "All");
